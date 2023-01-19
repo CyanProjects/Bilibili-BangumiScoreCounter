60678ab51e8c4d0b5197c55e079b03591eed5a37
Cyan <lc_shizuku@outlook.com>
2023-01-19 12:01:15 +0800

    更新了最新的 异化三体 评分数据(5.22>5.21) 命令行参数优化  - 支持使用参数设置url  - 支持使用参数加载存档  - 可以获取帮助  - 通过 -detail 获取详细信息  - 更新了相关文档 文档  - 添加 Update Log 信息 显示:  - 支持显示 所有番剧 剧集状态  - 如果传入了Episode Id可以显示此集的信息  - 优化颜色和缩进, 增强可读性  - 显示更多信息 其他:  - 修复无法自动生成 Season Url 的问题  - 修复自动生成的 Url 不包含 'https://' 的问题  - 支持解析http(s)和未设置的链接 画饼:  - 支持保存剧集信息  - 支持保存分数历史

--- README.md
+++ README.md
@@ -6,13 +6,14 @@
 #### 方法一
     git clone https://github.com/Chinese-Cyq20100313/Bilibili-BangumiScoreCounter.git sc_counter
     cd sc_counter
-    python ./main.py
+    ./main.py
 
 #### 方法二
 下载 [main.py](main.py) 直接使用
 
 ### 使用
 
+#### 无参数直接运行
 如果提示已有评分数据(项目自带异化版三体存档)  
 ![image](https://user-images.githubusercontent.com/68551684/213120402-0caa1565-7236-435d-a66e-a43b9435cff8.png)  
 并且你想要直接统计这个数据的评分，请输入Y并回车  
@@ -21,4 +22,40 @@
 如果没有提示，可以打开要统计的番剧，复制链接，粘贴，回车，等待获取结果。
 ![image](https://user-images.githubusercontent.com/68551684/213110498-c72a9aa0-0376-4eb3-8e3e-b55b589b7fba.png)
 
+#### 使用参数
+参数加载番剧Url
+    ./main.py -url [番剧URL]
+参数使用保存的数据
+    ./main.py -load
+获取帮助 (特性 只有help在最后才可以生效)
+    ./main.py -help
+番剧全部剧集信息
+    ./main.py -detail
+Example
+    ./main.py -detail -url https://www.bilibili.com/bangumi/play/ep704479?from_spmid=666.25.episode.0&from_outer_spmid=666.19.0.0
+
 #### 作者B站账号: Cyan_Changes UID475405591
+
+## Update Log
+23/1/19: 
+1. 更新了最新的 异化三体 评分数据(5.22>5.21)
+2. 命令行参数优化
+3.  - 支持使用参数设置url
+4.  - 支持使用参数加载存档
+5.  - 可以获取帮助
+6.  - 通过 -detail 获取详细信息
+7.  - 更新了相关文档
+8. 文档
+9.  - 添加 Update Log 信息
+10. 显示:
+11.  - 支持显示 所有番剧 剧集状态
+12.  - 如果传入了Episode Id可以显示此集的信息
+13.  - 优化颜色和缩进, 增强可读性
+14.  - 显示更多信息
+15. 其他:
+16.  - 修复无法自动生成 Season Url 的问题
+17.  - 修复自动生成的 Url 不包含 'https://' 的问题
+18.  - 支持解析http(s)和未设置的链接
+19. 画饼:
+20.  - 支持保存剧集信息
+21.  - 支持保存分数历史
--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-import sys
+import json
+import re
 from typing import Any
 
-from colorama import Fore, Back
 import requests
-
-import re
-
-import json
+from colorama import Fore, Back
 
 
 class Bilibili:
     """
     获取评分的迭代器(每次默认20个，不超过20个)
     """
+
     class CommentsIter:
-        def __init__(self, func, md_id: str, next: int = 0):
+        def __init__(self, func, md_id: int, max_cnt: int = 20, next: int = 0):
             self.next = next
             self.func = func
             self.md_id = md_id
+            self.max_cnt = max_cnt
 
         def __iter__(self):
             return self
 
         def __next__(self):
-            self.next, comments = self.func(md_id, self.next)
+            self.next, comments = self.func(md_id, max_cnt=self.max_cnt, next=self.next)
             if not comments:
                 raise StopIteration
             return comments
@@ -36,13 +35,14 @@ class Bilibili:
         :return: ep_url 和 ep_id 的 元组
         """
 
-        _ep_url_match = re.search("www.bilibili.com/bangumi/play/ep(?P<episode_id>\w*)", string, re.MULTILINE)
+        _ep_url_match = re.search("(http(s?)://)?www.bilibili.com/bangumi/play/ep(?P<episode_id>\w*)", string,
+                                  re.MULTILINE)
 
         if _ep_url_match:
             if len(_ep_url_match.regs) > 0:
                 _start, _end = _ep_url_match.regs[0]
                 _ep_url = string[_start:_end]
-                _ep_id: str = _ep_url_match.groupdict()['episode_id']
+                _ep_id: int = _ep_url_match.groupdict()['episode_id']
                 return _ep_url, _ep_id
 
     @classmethod
@@ -53,13 +53,14 @@ class Bilibili:
         :return: ss_url 和 ss_id 的 元组
         """
 
-        _ss_url_match = re.search("www.bilibili.com/bangumi/play/ss(?P<season_id>\w*)", string, re.MULTILINE)
+        _ss_url_match = re.search("(http(s?)://)?www.bilibili.com/bangumi/play/ss(?P<season_id>\w*)", string,
+                                  re.MULTILINE)
 
         if _ss_url_match:
             if len(_ss_url_match.regs) > 0:
                 _start, _end = _ss_url_match.regs[0]
                 _ss_url = string[_start:_end]
-                _ss_id: str = _ss_url_match.groupdict()['season_id']
+                _ss_id: int = _ss_url_match.groupdict()['season_id']
                 return _ss_url, _ss_id
 
     @classmethod
@@ -70,17 +71,18 @@ class Bilibili:
         :return: md_url 和 md_id 的 元组
         """
 
-        _md_url_match = re.search("www.bilibili.com/bangumi/media/md(?P<media_id>\w*)", string, re.MULTILINE)
+        _md_url_match = re.search("(http(s?)://)?www.bilibili.com/bangumi/media/md(?P<media_id>\w*)", string,
+                                  re.MULTILINE)
 
         if _md_url_match:
             if len(_md_url_match.regs) > 0:
                 _start, _end = _md_url_match.regs[0]
                 _md_url = string[_start:_end]
-                _md_id: str = _md_url_match.groupdict()['media_id']
+                _md_id: int = _md_url_match.groupdict()['media_id']
                 return _md_url, _md_id
 
     @classmethod
-    def get_season_status(cls, ss_id: str) -> dict | None:
+    def query_season_status(cls, ss_id: int) -> dict | None:
         """
         获取 Season 状态
         :param ss_id: Season Id
@@ -99,7 +101,7 @@ class Bilibili:
             return None
 
     @classmethod
-    def get_media_meta(cls, md_id: str) -> dict | None:
+    def query_media_meta(cls, md_id: int) -> dict | None:
         """
         获取 Media 元数据
         :param md_id: Media Id
@@ -119,7 +121,7 @@ class Bilibili:
             return None
 
     @classmethod
-    def query_short_comments_count(cls, md_id: str) -> int | None:
+    def query_short_comments_count(cls, md_id: int) -> int | None:
         """
         获取短评数
         :param md_id: Media Id
@@ -136,16 +138,18 @@ class Bilibili:
             return None
 
     @classmethod
-    def query_short_comments(cls, md_id: str, next: int = 0) -> tuple[int, list[dict[str, Any]]] | None:
+    def query_short_comments(cls, md_id: int, max_cnt: int = 20, next: int = 0)\
+            -> tuple[int, list[dict[str, Any]]] | None:
         """
         获取短评
+        :param max_cnt: 单次最大数: 20
         :param next:
         :param md_id: Media Id
         :return: 获取到的长评
         """
         _resp = requests.get("https://api.bilibili.com/pgc/review/short/list", params={
             "media_id": md_id,
-            "ps": 20,
+            "ps": max_cnt,
             "cursor": next
         })
         data = json.loads(_resp.text)
@@ -155,7 +159,7 @@ class Bilibili:
             return None
 
     @classmethod
-    def query_long_comments_count(cls, md_id: str) -> int | None:
+    def query_long_comments_count(cls, md_id: int) -> int | None:
         """
         获取长评数
         :param md_id: Media Id
@@ -172,16 +176,18 @@ class Bilibili:
             return None
 
     @classmethod
-    def query_long_comments(cls, md_id: str, next: int = 0) -> tuple[int, list[dict[str, Any]]] | None:
+    def query_long_comments(cls, md_id: int, max_cnt: int = 20, next: int = 0) -> tuple[
+                                                                                      int, list[dict[str, Any]]] | None:
         """
         获取短评
+        :param max_cnt: 单次最大数: 20
         :param next:
         :param md_id: Media Id
         :return: 获取到的长评
         """
         _resp = requests.get("https://api.bilibili.com/pgc/review/long/list", params={
             "media_id": md_id,
-            "ps": 20,
+            "ps": max_cnt,
             "cursor": next
         })
 
@@ -189,6 +195,44 @@ class Bilibili:
 
         if data['code'] == 0:
             return data['data']['next'], data['data']['list']
+        else:
+            return None
+
+    @classmethod
+    def query_episode_status(cls, ep_id: int) -> dict | None:
+        """
+        获取单集数据
+        :param ep_id: Episode Id
+        :return: 单集数据
+        """
+        _resp = requests.get("https://api.bilibili.com/pgc/season/episode/web/info", params={
+            "ep_id": ep_id
+        })
+        _resp.raise_for_status()
+
+        data = json.loads(_resp.text)
+
+        if data['code'] == 0:
+            return data['data']['stat']
+        else:
+            return None
+
+    @classmethod
+    def query_collective_info(cls, ss_id: int) -> dict | None:
+        """
+        获取番剧集合信息
+        :param ss_id: Season Id
+        :return: 集合信息
+        """
+        _resp = requests.get("https://api.bilibili.com/pgc/view/web/season", params={
+            "season_id": ss_id
+        })
+        _resp.raise_for_status()
+
+        data = json.loads(_resp.text)
+
+        if data['code'] == 0:
+            return data['result']
         else:
             return None
 
@@ -202,30 +246,112 @@ headers = {
 
 if __name__ == '__main__':
 
-    import os
-    import pathlib
+    def _at_exit():
+        print(Fore.RESET, Back.RESET, end='')
+
+
+    import sys
+
+
+    def exc_hook(exc_type, exc_value, tb):
+        import traceback
+        print(f"{Fore.LIGHTRED_EX}\nSomething went wrong: \n {Fore.LIGHTYELLOW_EX}<== <-- <-- · --> --> ==> {Fore.RED}")
+        import time
+        time.sleep(0.001)
+        traceback.print_exception(exc_type, exc_value, tb)
+
+
+    sys.excepthook = exc_hook
 
     load = False
-    if os.path.exists(pathlib.Path("long_comments.json")) \
-            and os.path.exists(pathlib.Path("short_comments.json")):
-        print(f"{Fore.YELLOW} 发现评分数据存档, 是否直接加载?")
-        ans = input("[Y/n] ").lower()
-        match ans:
-            case "n":
-                load = False
-            case default:
-                load = True
+    use_arg = (len(sys.argv) != 0)
+    URL: str = ''
+    detailed_epinfo = False
+
+
+    class ArgProc:
+        def __init__(self):
+            self.next_load = None
+
+        def __getitem__(self, item: str):
+            global load, URL, detailed_epinfo
+            if item.startswith(('-', '/')):
+                item = item[1:]
+                match item:
+                    case 'help':
+                        self.next_load = 'help'
+                        return True
+                    case 'url':
+                        self.next_load = 'load-url'
+                        return True
+                    case 'detail':
+                        detailed_epinfo = True
+                        return True
+                    case 'load':
+                        load = True
+                        return True
+                return False
+            else:
+                match self.next_load:
+                    case 'load-url':
+                        URL = item
+                        print(f"{Fore.CYAN} Selected Url: {Fore.LIGHTCYAN_EX}{URL}")
+                        self.next_load = None
+                        return True
+                self.next_load = None
+                return False
+
+
+    import os
+    import pathlib
+    import atexit
+
+    atexit.register(_at_exit)
+
+    print(f"{Fore.CYAN}Bangumi Score Counter - B站番剧真实评分计算")
+    print(f"{Fore.CYAN}作者 Cyan_Changes UID475405591")
+
+    parg = ArgProc()
+
+    if use_arg:
+        for arg in sys.argv[1:]:
+            if not parg[arg]:
+                print(f"{Fore.RED} Unknown argument {arg}")
+
+    if parg.next_load is not None:
+        if parg.next_load == 'help':
+            print(f"{Fore.LIGHTMAGENTA_EX} Bangumi Score Counter 用法")
+            print(f"{Fore.LIGHTYELLOW_EX} -url \t Bilibili Bangumi Page URL")
+            print(f"{Fore.LIGHTYELLOW_EX} -load \t Use Stored Comments")
+            print(f"{Fore.LIGHTYELLOW_EX} -help \t Show this help page")
+            sys.exit(0)
+        else:
+            print(f"{Fore.RED} There is not enough argument to process {parg.next_load}")
+            sys.exit(-1)
+
+    if (os.path.exists(pathlib.Path("long_comments.json")) and os.path.exists(pathlib.Path("short_comments.json"))) \
+            if not use_arg else load:
+        if not use_arg:
+            print(f"{Fore.YELLOW} 发现评分数据存档, 是否直接加载?")
+            ans = input("[Y/n] ").lower()
+            match ans:
+                case "n":
+                    load = False
+                case default:
+                    load = True
 
     long_comments = []
 
     shrt_comments = []
 
     if not load:
-        print(f"{Fore.MAGENTA} 请输入番剧主页 Url")
-        print(f"{Fore.LIGHTCYAN_EX}", end='')
-        URL: str = input(f"{Fore.LIGHTCYAN_EX}[URL] ")
 
-        print(f"{Fore.LIGHTMAGENTA_EX} 获取视频信息...")
+        if not URL or not use_arg:
+            print(f"{Fore.MAGENTA} 请输入番剧主页 Url")
+            print(f"{Fore.LIGHTCYAN_EX}", end='')
+            URL = input(f"{Fore.LIGHTCYAN_EX}[URL] ")
+
+        print(f"{Fore.LIGHTMAGENTA_EX} 获取番剧信息...")
 
         md_id, ss_id, ep_id = (None,) * 3
         md_url, ss_url, ep_url = (None,) * 3
@@ -246,7 +372,7 @@ if __name__ == '__main__':
                         print(f"{Fore.RED} URL 解析失败, 请检查链接是否有误!")
                         sys.exit(-1)
 
-        md_id, ss_id, ep_id = map((lambda x: 0 if not x else x), (md_id, ss_id, ep_id))
+        md_id, ss_id, ep_id = map((lambda x: 0 if not x else int(x)), (md_id, ss_id, ep_id))
 
         # request other id from api
         if not md_id:
@@ -260,11 +386,14 @@ if __name__ == '__main__':
                 if data['code'] == 0:
                     md_id = data['result']['media_id']
                     ss_id = data['result']['season_id']
+                    ss_url = f"https://www.bilibili.com/bangumi/play/ss{ss_id}"
+                    md_url = f"https://www.bilibili.com/bangumi/media/md{md_id}"
                 else:
                     print(f"{Fore.RED} Media 解析失败, 请检查链接是否有误!")
                     sys.exit(-1)
-
-            md_url = f"https://www.bilibili.com/bangumi/media/md{md_id}"
+            else:
+                print(f"{Fore.RED} Media 解析失败, 请检查链接是否有误!")
+                sys.exit(-1)
 
         if not ss_id:
             if md_id:
@@ -287,10 +416,11 @@ if __name__ == '__main__':
                     md_id = data['result']['season_id']
                 else:
                     print(f"{Fore.RED} Season 解析失败!")
-
+            else:
+                print(f"{Fore.RED} Season 解析失败!")
             ss_url = f"https://www.bilibili.com/bangumi/play/ss{ss_id}"
 
-        # Match id from page
+        # Match id from page [弃用]
         """
         _resp = requests.get(URL, headers=headers)
         _resp.raise_for_status()
@@ -299,16 +429,9 @@ if __name__ == '__main__':
         _, ep_id = Bilibili.grep_episode_id(_resp.text)
         """
 
-        bangumi_meta = Bilibili.get_media_meta(md_id)
-        season_stat = Bilibili.get_season_status(ss_id)
-
-        print(f"{Fore.CYAN} 番剧名称: {bangumi_meta['title']}")
-        print(f"{Fore.CYAN} 番剧类型: {bangumi_meta['type_name']}")
-        print(f"{Fore.CYAN} 番剧集数: {bangumi_meta['new_ep']['index_show']}")
-        print(f"{Fore.CYAN} 显示分数: {bangumi_meta['rating']['score']} ({bangumi_meta['rating']['count']})")
-        print(
-            f"{Fore.CYAN} 播放 {season_stat['views']} 投币 {season_stat['coins']} \t 弹幕 {season_stat['danmakus']} \n"
-            f" 追番 {season_stat['follow']} \t 系列追番 {season_stat['series_follow']} \n")
+        bangumi_meta = Bilibili.query_media_meta(md_id)
+        season_stat = Bilibili.query_season_status(ss_id)
+        collective_info = Bilibili.query_collective_info(ss_id)
 
         print(f"{Fore.CYAN} Episode Url: {ep_url}")
         print(f"{Fore.CYAN} Episode Id:  {ep_id}")
@@ -317,17 +440,46 @@ if __name__ == '__main__':
         print(f"{Fore.CYAN} Media Url:   {md_url}")
         print(f"{Fore.CYAN} Media Id:    {md_id}")
 
+        print(f"{Fore.CYAN} 番剧名称: {bangumi_meta['title']}")
+        print(f"{Fore.CYAN} 番剧类型: {bangumi_meta['type_name']}")
+        print(f"{Fore.CYAN} 番剧集数: {bangumi_meta['new_ep']['index_show']} {collective_info['new_ep']['desc']}")
+        print(f"{Fore.CYAN} 显示分数: {bangumi_meta['rating']['score']} ({bangumi_meta['rating']['count']})")
+        print(
+            f"{Fore.CYAN} 播放 {season_stat['views']} \t 投币 {season_stat['coins']} \t 弹幕 {season_stat['danmakus']} \n"
+            f" 追番 {season_stat['follow']} \t 系列追番 {season_stat['series_follow']}")
+
+        print(f"{Fore.LIGHTMAGENTA_EX} 正在获取剧集信息...")
+
+        print(f"{Fore.MAGENTA} Episode Info")
+
+        for episode in collective_info['episodes']:
+            if episode['id'] == ep_id or detailed_epinfo:
+                print(f"{Fore.LIGHTCYAN_EX}[{Fore.LIGHTBLUE_EX}长标题{Fore.LIGHTCYAN_EX}] "
+                      f"{Fore.CYAN}{episode['long_title'] if episode['long_title'] else f'{Fore.LIGHTBLACK_EX}(无)'}"
+                      f"{Fore.LIGHTBLACK_EX}({Fore.LIGHTBLUE_EX}ep{episode['id']}{Fore.LIGHTBLACK_EX}) \t\t"
+                      f"{Fore.LIGHTBLACK_EX}ts:{episode['pub_time']}\n"
+                      f"{Fore.LIGHTCYAN_EX}AId    \t{Fore.CYAN}{episode['aid']}\t "
+                      f"{Fore.LIGHTCYAN_EX}BVId   \t{Fore.CYAN}{episode['bvid']}\n"
+                      f"{Fore.LIGHTCYAN_EX}编号.   \t{Fore.CYAN}{episode['title']}\t "
+                      f"{Fore.LIGHTCYAN_EX}时长    \t{Fore.CYAN}{episode['duration']}")
+                episode_stat = Bilibili.query_episode_status(episode['id'])
+                print(f"{Fore.LIGHTCYAN_EX}播放 {Fore.CYAN}{episode_stat['view']} \t "
+                      f"{Fore.LIGHTCYAN_EX}点赞 {Fore.CYAN}{episode_stat['like']} \t "
+                      f"{Fore.LIGHTCYAN_EX}投币 {Fore.CYAN}{episode_stat['coin']} \n"
+                      f"{Fore.LIGHTCYAN_EX}弹幕 {Fore.CYAN}{episode_stat['dm']} \t "
+                      f"{Fore.LIGHTCYAN_EX}回复 {Fore.CYAN}{episode_stat['reply']}")
+
         shrt_cmt_cnt = Bilibili.query_short_comments_count(md_id)
 
         from tqdm import tqdm
 
         with tqdm(total=Bilibili.query_short_comments_count(md_id), desc=f"{Fore.RED}获取短评") as pbar:
-            for comments in iter(Bilibili.CommentsIter(Bilibili.query_short_comments, md_id)):
+            for comments in iter(Bilibili.CommentsIter(Bilibili.query_short_comments, md_id, 100)):
                 shrt_comments.extend(comments)
                 pbar.update(len(comments))
 
         with tqdm(total=Bilibili.query_long_comments_count(md_id), desc=f"{Fore.RED}获取长评") as pbar:
-            for comments in iter(Bilibili.CommentsIter(Bilibili.query_long_comments, md_id)):
+            for comments in iter(Bilibili.CommentsIter(Bilibili.query_long_comments, md_id, 100)):
                 long_comments.extend(comments)
                 pbar.update(len(comments))
 
